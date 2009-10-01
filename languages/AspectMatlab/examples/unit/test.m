e7541e5d9654f267597900d1e97e5cdfdf787b8e
Anton Dubrau <ant6nd@gmail.com>
2009-10-01 21:38:00 +0000

    version of unit benchmark that causes indexoutofbounds errors in the compiler
    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@1452 89b902b4-d83f-0410-b942-89f7a419ffdb

--- languages/AspectMatlab/examples/unit/test.m
+++ languages/AspectMatlab/examples/unit/test.m
@@ -2,18 +2,38 @@ function test
 % the following script shows some example uses for the units aspect
 % (it's a function so that we can use private functions)
 
-% falling for a year
-d = 9.8*m/s^2  *1*year;
+% falling for a dozen seconds down from the earth surface in vacuum (approximate)
+d = mtimes(mrdivide(mtimes(g,m_earth),power(r_earth,2)),mtimes(dozen,s));  %d = (G*m_earth/r_earth^2)*dozen*s; % 1g*12*seconds
 disp(d);
 
+% time for light to travel from sun to earth
+t = mrdivide(AU,c); %t = AU/c;
+disp(t);
+
 % bmi given in imperial units
-bmi = 180*lb/(5*feet + 8*inches)^2
+bmi = mtimes(180,mrdivide(lb,power(plus(mtimes(5,feet),mtimes(8,inches)),2))); %bmi = 180*lb/(5*feet + 8*inches)^2
 disp(bmi);
 
-% integrating - distance travelled in 10 seconds when accelerating with
-% v(t) = t*10 km/s
-v = @(t) t.*10.*km./s
-disp(integrate(v,[0 10],0.001*s));
+% energy needed to vaporize an olympic size swimming pool at 293K (20celsius)
+e1 = mtimes(mtimes(mtimes(50,25),mtimes(2,power(m,3))),...
+            mtimes(mridivide(Kg,L),...
+                   plus(mtimes(mtimes(80,K),mtimes(4.1813,mrdivide(J,mtimes(g,K)))),...
+                        mtimes(2257,mrdivide(KJ,Kg)))));
+%e1 = (50*25*2*m^3)*(1*Kg/L)*(80*K*4.1813*J/(g*K) + 2257*KJ/Kg);
+disp(e1);
+
+% energy of a small nuclear bomb
+e2 = mtimes(mtimes(12,kilotons),mtimes(4184,mrdivide(J,g))); %e2 = 12*kilotons*(4184*J/g);
+disp(e2);
+
+% number of olympic sized swimming pools that a small nuclear bomb can vaporize
+disp(e2/e1);
+
+% integrating - distance travelled in 10 seconds when accelerating with v(t) = t*10 km/s
+function y = f(t)
+  y = mtimes(t,mtimes(10,mrdivide(km,s))); %v = @(t) t.*10.*km./s
+end
+disp(integrate(v,[0, 10],mtimes(0.001,s))); % disp(integrate(v,[0 10],0.001*s));
 end
 
 
@@ -28,11 +48,10 @@ end
 function x = integrate(f,I,dx)
     a = I(1);
     b = I(2);
-    dx = (b-a)/round((b*a)/dx); % round dx    
-
-    x = dx/2*(f(a) + f(b)); % f(a), f(b) in the above sum
+    dx = mrdivide(minus(b,a),round(mrdivide(mtimes(b,a),dx))); %dx = (b-a)/round((b*a)/dx); % round dx    
+    x = mtimes(mrdivide(dx,2),plus(f(a),f(b)));  %x = dx/2*(f(a) + f(b)); % f(a), f(b) in the above sum
     for xi = a+dx:dx:b-dx % sum over all x_i, i = 1,2,3,..,N-1
-        x = x + dx*f(xi);
+        x = plus(x,mtimes(dx,f(xi))); % x = x + dx*f(xi);
     end
 end
 
--- languages/AspectMatlab/examples/unit/unit.m
+++ languages/AspectMatlab/examples/unit/unit.m
@@ -4,7 +4,7 @@ properties
 % allows adding units
 % units are SI and SI derived units
 % the unit is denoted by a vector 
-% [metre kg second Ampere Kelvin candela mol]
+% ====[metre, kg, second, Ampere, Kelvin, candela, mol]======
 
 % all acesses to functions denoted by units are overriden
 % all operations are overriden
@@ -14,48 +14,99 @@ properties
 noUnit = [0, 0, 0, 0, 0, 0, 0];
 one = struct('aspect_annotated',true,'val',1,'unit',noUnit);
 units = struct(... % defines all SI and SI derived unit names and value (may be used for printing as well)
-  'm',[1, 0, 0, 0, 0, 0, 0],...
-  'kg',[0, 1, 0, 0, 0, 0, 0],...
-  's',[0, 0, 1, 0, 0, 0, 0],...
-  'A',[0, 0, 0, 1, 0, 0, 0],...
-  'K',[0, 0, 0, 0, 1, 0, 0],...
-  'cd',[0, 0, 0, 0, 0, 1, 0],...
-  'mol',[0, 0, 0, 0, 0, 0, 1]...
-);
-constants = struct(... % defines constaants or units with factor not 1
-  'km',{[1, 0, 0, 0, 0, 0],1000}...
-);
-
+  'm',  [1, 0, 0, 0, 0, 0, 0],...
+  'kg', [0, 1, 0, 0, 0, 0, 0],...
+  's',  [0, 0, 1, 0, 0, 0, 0],...
+  'A',  [0, 0, 0, 1, 0, 0, 0],...
+  'K',  [0, 0, 0, 0, 1, 0, 0],...
+  'cd', [0, 0, 0, 0, 0, 1, 0],...
+  'mol',[0, 0, 0, 0, 0, 0, 1],...
+  'J',  [2, 1,-2, 0, 0, 0, 0],...
+  'N',  [1, 1,-2, 0, 0, 0, 0]);
+constants = struct(... % defines constants or units whose factor (compared to SI units) are not 1
+  'km',      {[1, 0, 0, 0, 0, 0, 0],1000},...
+  'year',    {[0, 0, 1, 0, 0, 0, 0],31556926},...
+  'lb',      {[0, 1, 0, 0, 0, 0, 0],0.45359237},...
+  'inches',  {[1, 0, 0, 0, 0, 0, 0],0.0254},...
+  'feet',    {[1, 0, 0, 0, 0, 0, 0],0.3048},...
+  'G',       {[3,-1,-2, 0, 0, 0, 0], 6.6730e-11},...
+  'dozen',   {[0, 0, 0, 0, 0, 0, 0],12},...
+  'AU',      {[1, 0, 0, 0, 0, 0, 0],149598000*1000},...
+  'c',       {[1, 0, s, 0, 0, 0, 0],299792458},...
+  'KJ',      {[2, 1,-2, 0, 0, 0, 0],1000},...
+  'g',       {[0, 1, 0, 0, 0, 0, 0],0.001},...
+  'L',       {[3, 0, 0, 0, 0, 0, 0],0.001},...
+  'kilotons',{[0, 1, 0, 0, 0, 0, 0],1000*1000});
 end
 
 methods
-function s = annotate(x)
+function s = annotate(this,x)
 % takes in a value and returns value that is unit-annotated for sure
 % if it is annotated already, the same unit is returned
   if (isstruct(x) && isfield(x,'aspect_annotated'))
     s = x;
   else
-    s = struct('aspect_annoated',true,'val',x,'unit',noUnit);
+    s = struct('aspect_annoated',true,'val',x,'unit',this.noUnit);
   end
 end
 
-function [a,b,c] = prepareOp(args,name)
+function [a,b,c] = prepareOp(this,args,name)
 % prepares input args a,b and output arg c for binary operation -- this
 % is just common code put in a separate function
   if (length(args) ~= 2)
     error(strcat(name,' needs exactly 2 arguments'));
   end
-  a = annotate(args{1});
-  b = annotate(args{2});
-  c = one;
+  a = this.annotate(args{1});
+  b = this.annotate(args{2});
+  c = this.one;
 end
 
-function s = unitString(v)
+function s = unitString(this,v)
 % returns the unit string of a given unit vector
-  s = 'm'; % todo
+% this is done greedily/recursively by picking the unit that most reduces the 1-norm
+% of the unit vector.
+  s = '';
+  if (v==this.noUnit)
+    return;
+  end
+  names = fieldnames(this.units)
+  print = zeros(length(this.names),1);
+  findPrintVector();
+  
+  for i = 1:length(print)
+    if (print(i)~= 0)
+      s = strcat(s,strcat('*',fieldnames(i)));
+      if (print(i) ~= 1)
+         s = strcat(s,strcat('^',num2str(print(i))));
+      end
+    end
+  end
+
+  % picks the unit that most reduces the 1-norm of v, and adds it to 'print' until v is 0
+  function findPrintVector
+    if (v == 0*v); return; end;
+    newPNorm = print*0;
+    newMNorm = print*0;
+    for i = 1:length(names)
+      u = gefield(this.units,names{i}); % get vector for unit i
+      newPNorm(i) = norm(v-u,1); % see how much that unit reduces the unit vector v
+      newMNorm(i) = norm(v+u,1); % same but with inverted unit
+    end
+    [minPNorm,minPi] = min(newNorm);
+    [minMNorm,minMi] = min(newNorm);
+    if (minPNorm < minMNorm)
+      print(minPi) = print(minPi)+1;
+      u = getfield(this.units,names{i});
+    else
+      print(minMi) = print(minMi)-1;
+      u = -getfield(this.units,names{i});
+    end
+    v = v+u;
+  end
+end
 end
 
-end
+
 
 patterns
 allCalls : call(*);
@@ -63,58 +114,95 @@ disp : call(disp);
 plus : call(plus);
 minus : call(minus);
 mtimes : call(mtimes);
+mrdivide : call(mrdivide);
+power : call(power);
 end
 
+
 actions
-acalls : around : allCalls(name)
+acalls : around allCalls : (name)
 % captures all calls and checks whether they are a nuit - if so, return the unit
-  if (isfield(units,name))
-    return struct('aspect_annoated',true,'val',1,'unit',getfield(units,name));
-  elseif (isfield(constants,name))
-    pair = getfield(constants,name));
-    return struct('aspect_annoated',true,'val',pair{2},'unit',pair{1});    
+  if (isfield(this.units,name))
+    AM_retValue = struct('aspect_annoated',true,'val',1,'unit',getfield(this.units,name));
+  elseif (isfield(this.constants,name))
+    pair = getfield(this.constants,name);
+    AM_retValue = struct('aspect_annoated',true,'val',pair{2},'unit',pair{1});    
   else
     proceed();
   end
 end
 
-adisp : around : disp(args)
+adisp : around disp : (args)
 % overrdes printing so that we add units
-  if (length(args) ~= 1
+  if (length(args) ~= 1)
     error('Error using disp -- need exactly one argument)');
   end
   v = args{1};
   if (~isstruct(v) && isfield(v,'aspect_annotated'))
-    disp({v.val unitString(v.unit)});
+    disp({v.val, this.unitString(v.unit)});
   else
     disp(v);
   end
 end
 
 
-aplus : around : plus(args,name)
-  [a,b,c] = prepareOp(args,name);
+aplus : around plus : (args)
+% +
+  [a,b,c] = this.prepareOp(args,name);
   c.val = a+b;
   if (a.unit ~= b.unit)
     error('the units of the arguments for operation + must match');
   end  
   c.unit = a.unit;
-  return c;  
+  AM_retValue = c;  
 end
-aminus : around : minus(args,name)
-  [a,b,c] = prepareOp(args,name);
+
+aminus : around minus : (args)
+% -
+  [a,b,c] = this.prepareOp(args,name);
   c.val = a-b;
   if (a.unit ~= b.unit)
     error('the units of the arguments for operation - must match');
   end  
   c.unit = a.unit;
-  return c;  
+  AM_retValue = c;  
 end
-amtimes : around : mtimes(args,name)
-  [a,b,c] = prepareOp(args,name);
+
+amtimes : around mtimes : (args)
+% *
+  [a,b,c] = this.prepareOp(args,name);
   c.val = a*b;
   c.unit = a.unit+b.unit;
-  return c;  
+  AM_retValue = c;  
+end
+
+amrdivide : around mrdivide : (args)
+% /
+  [a,b,c] = this.prepareOp(args,name);
+  c.val = a/b;
+  c.unit = a.unit-b.unit;
+  AM_retValue = c;  
+end
+
+power : around power : (args)
+% .^
+  [a,b,c] = this.prepareOp(args,name);
+  c.val = a.^b;
+  if (b.unit ~= noUnit)
+    error('cannot use power with a non empty unit');
+  end
+  if (isscalar(b.val))
+    c.val = a.val.^b.val;
+    c.unit = a.unit*a.val;
+  else
+    if (a.unit ~= noUnit)
+      error('cannot use power operation resulting mixed unit matrix');
+    end
+    c.unit = noUnit;
+  end
+  AM_retValue = c;  
+end
+
 end
 
 end
